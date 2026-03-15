15a99919ad2cd5c0b2fb678c28bf97ebeea23818
meepstertron <meepstertron@gmail.com>
2026-03-15 13:18:40 +0100

    fixed tailwind shenanigans

--- racked-website/src/components/panel.tsx
+++ racked-website/src/components/panel.tsx
@@ -2,7 +2,7 @@ import Screw from "./screw";
 
 function Panel({units, children, filler}: {units: number, children?: React.ReactNode, filler?: boolean}) {
     return ( 
-        <div className={`bg-white w-full h-${units * 32} rounded-md px-12 flex flex-row justify-between items-center${filler ? " invisible" : ""}`}>
+        <div className={`bg-white w-full rounded-md px-12 flex flex-row justify-between items-center${filler ? " invisible" : ""}`} style={{height: `${units * 8}rem`}}>
             <div className={"flex flex-col h-full py-5" + (units > 1 ? " justify-between" : " justify-center")}>{Array.from({ length: units }, (_, i) => <Screw key={i} />)}</div>
             <div className="w-full h-full">{children}</div>
             <div className={"flex flex-col h-full py-5" + (units > 1 ? " justify-between" : " justify-center")}>{Array.from({ length: units }, (_, i) => <Screw key={i} />)}</div>
